3c25fff86f42aaba8bea036c56d99d7a1e8320f2
Yasin Shamrat <yshamrat@gmail.com>
2020-01-22 00:51:39 +0600

    Main UI from xd is implemented.

--- MrQuotes/FirstViewController.swift
+++ MrQuotes/FirstViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "quoteCell", for: indexPath)
+        return cell
     }
 
 
