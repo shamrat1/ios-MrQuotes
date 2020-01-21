883e4f56c4e294437f1f98ff4195747782f37aad
Yasin Shamrat <yshamrat@gmail.com>
2020-01-21 21:32:59 +0600

    Initial Commit

---
+++ MrQuotes/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MrQuotes
+//
+//  Created by Yasin Shamrat on 21/1/20.
+//  Copyright © 2020 Yasin Shamrat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MrQuotes/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MrQuotes
+//
+//  Created by Yasin Shamrat on 21/1/20.
+//  Copyright © 2020 Yasin Shamrat. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
